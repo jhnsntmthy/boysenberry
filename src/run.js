3da5e62c30532b26e01866823c22e610d72cc6f3
Harambe <jhnsntmthy@gmail.com>
2016-12-11 17:33:32 -0800

    not much, just most

--- src/run.js
+++ src/run.js
@@ -1,9 +1,20 @@
 // @flow
 import _ from 'lodash';
 import $ from 'jquery';
+import mo from 'most';
+
 var str:string = 'hello world!';
 console.log(str);
 
 import component from './play/component_hello'
 
 document.body.appendChild(component());
+const writetoDom = (str) => {
+  $("#some").html("Character " + str);
+}
+
+mo.from([1,2,3,4])
+  .map(x => String.fromCharCode(x + 72))
+  .tap(x => console.log(x))
+  .tap(x => writetoDom(x))
+  .subscribe({});

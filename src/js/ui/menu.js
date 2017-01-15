a850447df8bfe543295905fddb7f5b784d4bdac5
Tim Johnson <tjohnson@scitent.com>
2017-01-15 18:09:36 -0500

    updates for the menu with most streams

--- src/js/ui/menu.js
+++ src/js/ui/menu.js
@@ -1,5 +1,21 @@
 /* eslint-env browser, jquery */
 import $ from 'jquery';
+import * as most from 'most';
+
+// const menuLink = document.querySelector('.click-toggle-overlay-menu');
+// const overlay = document.querySelector('.overlay');
+
+const toggleMenu = () => {
+  $('.overlay').fadeToggle(200);
+  $('.button a').toggleClass('btn-open').toggleClass('btn-close');
+};
+
+most.fromEvent('click', document.querySelector('.click-toggle-overlay-menu'))
+    .observe(e => toggleMenu(e));
+
+most.fromEvent('click', document.querySelector('.overlay'))
+    .observe(e => toggleMenu(e));
+
 
 // $('.click-toggle-menu').on('click', $.debounce(250, () => {
 //   $('.overlay').fadeToggle(200);

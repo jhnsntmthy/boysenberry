170abe9da3c7f476954ea183a70f7f336300a1fa
Tim Johnson <tjohnson@scitent.com>
2017-01-15 18:10:12 -0500

    updated and refactored some hucklebuck buttons and previous/next events

--- src/js/lib/sections/hucklebuck.js
+++ src/js/lib/sections/hucklebuck.js
@@ -4,16 +4,16 @@ import './hucklebuck_buttons';
 // import createHistory from './prehistoric/history';
 
 // const { push, history } = createHistory();
-
 // history.observe(dat => console.log('HISTORICAL', dat));
 
 const sections_prefetched = {};
-const sections_order = {};
+const sections_order = [];
+window.current_section = '';
+
 $(document).on('section::loaded', (e, slug, order, body) => {
   sections_prefetched[slug] = body;
   sections_order[order] = slug;
 });
-
 
 const renderSectionBySlug = (slug) => {
   console.log(`Rendering Section by Slug: ${slug}`);
@@ -21,26 +21,54 @@ const renderSectionBySlug = (slug) => {
     // push('/'+slug, { state: slug });
     document.getElementById('content').innerHTML = sections_prefetched[slug];
     $(document).trigger('hucklebucked', [slug]);
+    return slug;
   } else {
     document.getElementById('content').innerHTML = sections_prefetched['404'];
     $(document).trigger('404page');
+    return '404';
   }
 };
 
 const renderSectionByNumber = (i) => {
   console.log(`Rendering Section by #${i}`);
-  renderSectionBySlug(sections_order[i]);
+  return renderSectionBySlug(sections_order[i]);
+};
+
+const renderSectionNext = (slug) => {
+  const index = sections_order.indexOf(slug);
+  if (index > sections_order - 1) return slug;
+  return renderSectionBySlug(sections_order[index + 1]);
+};
+
+const renderSectionPrev = (slug) => {
+  const index = sections_order.indexOf(slug);
+  if (index <= 0) return slug;
+  return renderSectionBySlug(sections_order[index - 1]);
 };
 
 const hucklebuck = (slug_or_num) => {
+  let slug = slug_or_num;
   if (Number.isInteger(slug_or_num)) {
-    renderSectionByNumber(slug_or_num);
+    slug = renderSectionByNumber(slug_or_num);
+  } else if (slug_or_num === 'next') {
+    slug = renderSectionNext(window.current_section);
+  } else if (slug_or_num === 'prev') {
+    slug = renderSectionPrev(window.current_section);
   } else {
-    renderSectionBySlug(slug_or_num);
+    slug = renderSectionBySlug(slug);
   }
-  $(document).trigger('post-hucklebucked', [slug_or_num]);
+  $(document).trigger('post-hucklebucked', [slug]);
 };
 
 $(document).on('hucklebuck', (event, slug_or_num) => {
+  console.log('hucklebuck called', slug_or_num);
   hucklebuck(slug_or_num);
 });
+
+$(document).on('hucklebucked', (e, slug) => {
+  window.current_section = slug;
+});
+
+window.hucklebuck = (slug_or_num) => {
+  $(document).trigger('hucklebuck', [slug_or_num]);
+};
--- src/js/lib/sections/hucklebuck_buttons.js
+++ src/js/lib/sections/hucklebuck_buttons.js
@@ -1,38 +1,30 @@
+/* eslint-env browser, jquery */
 import $ from 'jquery';
 
-$(document).on('click','[data-nav]', function(event){
-  var next = $(event.target).closest('[data-nav]').attr('data-nav');
+$(document).on('click', '[data-nav]', (event) => {
+  const next = $(event.target).closest('[data-nav]').attr('data-nav');
   $(document).trigger('hucklebuck', [next]);
 });
 
-$(document).on('click','#next-up', function(event){
-  var next = $(event.target).attr('data-next-up');
+$(document).on('click', '#next-up', (event) => {
+  const next = $(event.target).attr('data-next-up');
   $(document).trigger('hucklebuck', [next]);
 });
 
-$(document).on('click', '.cust-btn', function(event){
-  var $target = $(event.currentTarget);
-  var next = $target.attr('data-target');
+$(document).on('click', '.cust-btn', (event) => {
+  const $target = $(event.currentTarget);
+  const next = $target.attr('data-target');
   $(document).trigger('hucklebuck', [next]);
 });
 
-$(document).on('click', '.trigger-next', function(event){
-  if(progress['currentPage'] === 'page1'){
-    $(document).trigger('hucklebuck', ['page2']);
-  } else if(progress['currentPage'] === 'page2'){
-    $(document).trigger('hucklebuck', ['page3']);
-  }
-});
-$(document).on('click', '.trigger-back', function(event){
-  if(progress['currentPage'] === 'page4'){
-    $(document).trigger('hucklebuck', ['page3']);
-  } else if(progress['currentPage'] === 'page3'){
-    $(document).trigger('hucklebuck', ['page2']);
-  } else if(progress['currentPage'] === 'page2'){
-    $(document).trigger('hucklebuck', ['page1']);
-  }
+$(document).on('click', '.trigger-next', () => {
+  $(document).trigger('hucklebuck', ['next']);
 });
 
-$(document).on('touchstart', 'a', function(event){
-  $(event.target).trigger('click')
+$(document).on('click', '.trigger-back', () => {
+  $(document).trigger('hucklebuck', ['prev']);
+});
+
+$(document).on('touchstart', 'a', (event) => {
+  $(event.target).trigger('click');
 });
